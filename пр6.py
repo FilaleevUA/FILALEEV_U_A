70921da7423bcba0ebfae7919f75890653d9a8ff
FilaleevUA <113377230+FilaleevUA@users.noreply.github.com>
2022-11-04 15:39:21 +0300

    пр

--- пр6.py
+++
@@ -1,9 +0,0 @@
-def pr7():
-    s = input('Введите строку \n')
-    str_split = s.split(' ')
-    for i in range(len(str_split)):
-        for i2 in range(len(str_split[i])):
-            if str_split[i][i2]=='я' or str_split[i][i2]=='Я':
-                print(str_split[i])
-
-pr7()
